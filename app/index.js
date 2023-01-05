662a15523352bb9185f8d4771f8257871aa637ba
jobscale <jobscale@users.noreply.github.com>
2023-01-06 05:32:33 +0900

    app updated

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const url = 'https://finance.yahoo.co.jp/quote/{{code}}';
 
 class Kabuka {
   fetch(code) {
-    if (Array.isArray()) return Promise.all(code.map(c => this.fetch(c)));
+    if (Array.isArray(code)) return Promise.all(code.map(c => this.fetch(c)));
     const uri = url.replace(/{{code}}/, code);
     return fetch.get(uri)
     .then(res => new JSDOM(res.data).window.document)
