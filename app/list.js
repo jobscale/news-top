280b57dc38c91d7a3da1df5c26b8f5e217517150
jobscale <jobscale@users.noreply.github.com>
2024-06-27 10:20:06 +0900

    amz updated

--- app/list.js
+++ app/list.js
@@ -7,9 +7,6 @@ const list = [
 ];
 
 const amz = [
-  { name: 'DDR 64GB', sale: 17000, uri: 'https://www.amazon.co.jp/gp/product/B07ZLCVKPV' },
-  { name: 'DDR 64GB', sale: 17000, uri: 'https://www.amazon.co.jp/gp/product/B09578GQF1' },
-  { name: 'DDR 64GB', sale: 17000, uri: 'https://www.amazon.co.jp/gp/product/B08XBCQXDV' },
   { name: 'DICE BANK MBK ', sale: 17000, uri: 'https://www.amazon.co.jp/gp/product/B0C4THDKMD' },
   { name: 'DICE BANK CAMO', sale: 17000, uri: 'https://www.amazon.co.jp/gp/product/B0C4THSTS1' },
   { name: 'DICE BANK DMSM', sale: 17000, uri: 'https://www.amazon.co.jp/gp/product/B0C4TJ6YBB' },
