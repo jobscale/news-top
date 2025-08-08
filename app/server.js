1ed25a1ff8be7c7fac1f70c3d482f2651f6c4998
jobscale <jobscale@users.noreply.github.com>
2025-08-09 00:49:58 +0900

    servers updated

--- app/server.js
+++ app/server.js
@@ -12,19 +12,19 @@ export const servers = {
     model: 'gemma-3-4b-it',
   },
   'local-gemma-3': {
-    endpoint: 'http://127.0.0.1:2880/v1/chat/completions',
+    endpoint: 'http://172.16.6.77:2880/v1/chat/completions',
     model: 'gemma-3-4b-it',
   },
   'local-Phi-4': {
-    endpoint: 'http://127.0.0.1:2881/v1/chat/completions',
+    endpoint: 'http://172.16.6.77:2881/v1/chat/completions',
     model: 'Phi-4-mini-reasoning',
   },
   'local-calm2': {
-    endpoint: 'http://127.0.0.1:2882/v1/chat/completions',
+    endpoint: 'http://172.16.6.77:2882/v1/chat/completions',
     model: 'calm2-7B-chat',
   },
   'local-ELYZA': {
-    endpoint: 'http://127.0.0.1:2883/v1/chat/completions',
+    endpoint: 'http://172.16.6.77:2883/v1/chat/completions',
     model: 'Llama-3-ELYZA-JP-8B',
   },
 };
