3ce46f178465f6c1ecd4bc6c7395a7caff1420f0
jobscale <jobscale@users.noreply.github.com>
2025-08-10 08:42:24 +0900

    servers updated

--- app/server.js
+++ app/server.js
@@ -1,30 +1,42 @@
 export const servers = {
   prod: {
     endpoint: 'https://llama.x.jsx.jp/v1/chat/completions',
-    model: 'gemma-3-4b-it',
+    model: 'Gemma-3n-E2B-it',
   },
   'in-lms': {
     endpoint: 'http://in.jsx.jp:1234/v1/chat/completions',
-    model: 'gemma-3-4b-it',
+    model: 'Gemma-3-4B-it',
   },
-  'n100-llama': {
+  'n100-gemma-3n': {
     endpoint: 'http://n100.jsx.jp:2880/v1/chat/completions',
-    model: 'gemma-3-4b-it',
+    model: 'Gemma-3n-E2B-it',
+  },
+  'n100-gemma-3': {
+    endpoint: 'http://n100.jsx.jp:2881/v1/chat/completions',
+    model: 'Gemma-3-4B-it',
+  },
+  'n100-gemma-3-Q5': {
+    endpoint: 'http://n100.jsx.jp:2882/v1/chat/completions',
+    model: 'Gemma-3-4B-it',
+  },
+  'local-gemma-3n': {
+    endpoint: 'http://172.16.6.77:2880/v1/chat/completions',
+    model: 'Gemma-3n-E2B-it',
   },
   'local-gemma-3': {
-    endpoint: 'http://172.16.6.77:2880/v1/chat/completions',
-    model: 'gemma-3-4b-it',
+    endpoint: 'http://172.16.6.77:2881/v1/chat/completions',
+    model: 'Gemma-3-4B-it',
+  },
+  'local-gemma-3-Q5': {
+    endpoint: 'http://172.16.6.77:2882/v1/chat/completions',
+    model: 'Gemma-3-4B-it',
   },
   'local-Phi-4': {
-    endpoint: 'http://172.16.6.77:2881/v1/chat/completions',
+    endpoint: 'http://172.16.6.77:2887/v1/chat/completions',
     model: 'Phi-4-mini-reasoning',
   },
-  'local-calm2': {
-    endpoint: 'http://172.16.6.77:2882/v1/chat/completions',
-    model: 'calm2-7B-chat',
-  },
   'local-ELYZA': {
-    endpoint: 'http://172.16.6.77:2883/v1/chat/completions',
+    endpoint: 'http://172.16.6.77:2888/v1/chat/completions',
     model: 'Llama-3-ELYZA-JP-8B',
   },
 };
