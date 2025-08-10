904a487e9c189dfe7ee1254f413586f0bdbbb1e4
jobscale <jobscale@users.noreply.github.com>
2025-08-10 21:13:46 +0900

    servers updated

--- app/server.js
+++ app/server.js
@@ -1,41 +1,52 @@
 export const servers = {
   prod: {
     endpoint: 'https://llama.x.jsx.jp/v1/chat/completions',
-    model: 'Gemma-3n-E2B-it',
+    model: 'Gemma-3n-E4B-it',
   },
   'in-lms': {
     endpoint: 'http://in.jsx.jp:1234/v1/chat/completions',
-    model: 'Gemma-3-4B-it',
+    model: 'Gemma-3n-E4B-it',
   },
-  'n100-gemma-3n': {
+
+  'n100-gemma-3n-E4B-Q5': {
     endpoint: 'http://n100.jsx.jp:2880/v1/chat/completions',
-    model: 'Gemma-3n-E2B-it',
+    model: 'Gemma-3n-E4B-it',
   },
-  'n100-gemma-3': {
+  'n100-gemma-3n-E4B-Q4': {
     endpoint: 'http://n100.jsx.jp:2881/v1/chat/completions',
-    model: 'Gemma-3-4B-it',
+    model: 'Gemma-3n-E4B-it',
   },
-  'n100-gemma-3-Q5': {
+  'n100-gemma-3n-E2B-Q5': {
     endpoint: 'http://n100.jsx.jp:2882/v1/chat/completions',
-    model: 'Gemma-3-4B-it',
-  },
-  'local-gemma-3n': {
-    endpoint: 'http://172.16.6.77:2880/v1/chat/completions',
     model: 'Gemma-3n-E2B-it',
   },
-  'local-gemma-3': {
-    endpoint: 'http://172.16.6.77:2881/v1/chat/completions',
-    model: 'Gemma-3-4B-it',
+  'n100-gemma-3n-E2B-Q4': {
+    endpoint: 'http://n100.jsx.jp:2883/v1/chat/completions',
+    model: 'Gemma-3n-E2B-it',
   },
-  'local-gemma-3-Q5': {
-    endpoint: 'http://172.16.6.77:2882/v1/chat/completions',
-    model: 'Gemma-3-4B-it',
+
+  'dark-gemma-3n-E4B-Q5': {
+    endpoint: 'http://dark.jsx.jp:2880/v1/chat/completions',
+    model: 'Gemma-3n-E4B-it',
   },
+  'dark-gemma-3n-E4B-Q4': {
+    endpoint: 'http://dark.jsx.jp:2881/v1/chat/completions',
+    model: 'Gemma-3n-E4B-it',
+  },
+  'dark-gemma-3n-E2B-Q5': {
+    endpoint: 'http://dark.jsx.jp:2882/v1/chat/completions',
+    model: 'Gemma-3n-E2B-it',
+  },
+  'dark-gemma-3n-E2B-Q4': {
+    endpoint: 'http://dark.jsx.jp:2883/v1/chat/completions',
+    model: 'Gemma-3n-E2B-it',
+  },
+
   'local-Phi-4': {
     endpoint: 'http://172.16.6.77:2887/v1/chat/completions',
     model: 'Phi-4-mini-reasoning',
   },
-  'local-ELYZA': {
+  'local-Llama-3': {
     endpoint: 'http://172.16.6.77:2888/v1/chat/completions',
     model: 'Llama-3-ELYZA-JP-8B',
   },
