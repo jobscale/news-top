372044b88d9ca99a9812b5dcc4ba3b794ed774fc
jobscale <jobscale@users.noreply.github.com>
2025-08-11 07:43:59 +0900

    get-item added

---
+++ get-item/index.js
@@ -0,0 +1,32 @@
+import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
+import { unmarshall, marshall } from '@aws-sdk/util-dynamodb';
+
+const logger = console;
+const client = new DynamoDBClient({
+  endpoint: 'http://n100.jsx.jp:4566',
+  region: 'ap-northeast-1',
+  credentials: {
+    accessKeyId: 'test',
+    secretAccessKey: 'test',
+  },
+});
+
+const run = async () => {
+  const data = await client.send(new GetItemCommand({
+    TableName: 'News',
+    Key: marshall({ Title: 'history' }),
+  }));
+
+  const item = data?.Item && unmarshall(data.Item);
+  if (!item) return;
+
+  const filtered = item.history
+  // .filter(entry => Number.isInteger(entry?.score) && entry.score >= 5)
+  // .filter(entry => !entry?.deny)
+  // .filter(entry => entry?.emergency)
+  .slice(-100);
+
+  logger.info(JSON.stringify(filtered, null, 2));
+};
+
+run().catch(logger.error);
