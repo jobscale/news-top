e1b0b1a7d0a80e4228bea9f4d496ee011cc5efc0
jobscale <jobscale@users.noreply.github.com>
2025-08-30 18:12:09 +0900

    llm score updated

--- app/llm.js
+++ app/llm.js
@@ -11,13 +11,13 @@ export const aiCalc = async title => {
   const sum = { subjectivity: 0, penalty: 0 };
   sum.penalty += Math.max(0, logic.noisy.length - logic.emergency.length * 2);
   sum.penalty += dataset.noisy.filter(word => ai.influence?.includes(word)).length;
-  const inverse = v => (5 - v) / 4;
+  const inverse = v => (5 - v) * 0.25;
   sum.subjectivity += inverse(ai.credibility);
   sum.subjectivity += inverse(ai.importance);
   sum.subjectivity += inverse(ai.urgency);
   sum.subjectivity += inverse(ai.novelty);
-  sum.subjectivity += ai.bias / 2;
-  sum.sensitivity = ai.negative < ai.positive ? ai.positive : ai.negative * -1;
+  sum.subjectivity += ai.bias * 0.5;
+  sum.sensitivity = ai.positive - ai.negative;
   delete ai.positive;
   delete ai.negative;
   sum.preliminary = ai.newsworthiness + ai.impact - sum.subjectivity;
